61d7dd11e6f1b0c02b88bf342b9e96c65520a709
Eugene <ti.eugene@gmail.com>
2020-09-20 19:19:58 +0300

    get_addrs run ok

--- get_addrs.py
+++ get_addrs.py
@@ -1,20 +1,22 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 """
 Get all blockchain addresses
 """
 
+import sys
 from btc.authproxy import AuthServiceProxy as Proxy
-from btc.utils import load_conf, eprint, Timer
+from btc.utils import load_conf, pk2addr
 
 Dup_Blocks = {91722, 91812}  # duplicate 91880, 91842
 
 
-def walk():
+def walk(qty: int = 0):
     rpc_connection = Proxy(load_conf(), timeout=300)  # for heavy load
     bk_hash = rpc_connection.getblockhash(0)
-    bk_count = rpc_connection.getblockcount()
+    if qty == 0:
+        qty = rpc_connection.getblockcount()
     # 1. go
-    for bk_no in range(0, 5):
+    for bk_no in range(0, qty):
         bk = rpc_connection.getblock(bk_hash, 2)
         if bk_no in Dup_Blocks:
             continue
@@ -22,15 +24,28 @@ def walk():
         for tx in bk['tx']:
             for vout in tx['vout']:
                 vout_no = vout['n']
-                addrs = vout.get('addresses', None)
-                pfx = "{}\t{}\t{}\t{}".format(bk_no, tx_no, vout_no, vout['scriptPubKey']['type'])
-                if addrs:
-                    print("{}\t{}".format(pfx, ','.join(addrs)))
+                spk = vout['scriptPubKey']
+                spk_type = spk['type']
+                if spk_type == 'pubkey':
+                    addr_l = [pk2addr(spk['asm'].split(' ')[0])]
+                else:
+                    addr_l = spk.get('addresses', None)
+
+                pfx = "{}\t{}\t{}\t{}".format(bk_no, tx_no, vout_no, spk_type)
+                if addr_l:
+                    print("{}\t{}".format(pfx, ','.join(addr_l)))
                 else:
                     print(pfx)
             tx_no += 1
-        bk_hash = block['nextblockhash']
+        bk_hash = bk['nextblockhash']
 
 
 if __name__ == '__main__':
-    walk()
+    qty = 0
+    if len(sys.argv) == 2:
+        if sys.argv[1].isdigit():
+            qty = int(sys.argv[1])
+        else:
+            print("Usage: {} [qty[=all]".format(sys.argv[0]))
+            exit()
+    walk(qty)
